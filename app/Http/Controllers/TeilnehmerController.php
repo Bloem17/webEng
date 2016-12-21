e7c647d1c104a35dfea2a691b873588a832b2505
macbook AIR <macbookAIR@laptop-gianni.edu.ds.fhnw.ch>
2016-12-21 16:20:18 +0100

    idk

--- app/Http/Controllers/TeilnehmerController.php
+++ app/Http/Controllers/TeilnehmerController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\CreateTeilnehmerRequest;
-use \App\models\Teilnehmer;
-use \App\models\Reise;
+use App\models\Teilnehmer;
+use App\models\Reise;
 use View;
 
 class TeilnehmerController extends Controller
